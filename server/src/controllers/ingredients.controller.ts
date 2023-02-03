773a2b015feafc6830dfc2f1bc74d62cbb523585
Danil Nesterenko <nochzato@gmail.com>
2023-02-04 00:06:52 +0200

    added types to mongoose error and documents

--- server/src/controllers/ingredients.controller.ts
+++ server/src/controllers/ingredients.controller.ts
@@ -1,16 +1,18 @@
-import { Request, Response } from 'express';
 import _ from 'lodash';
+import { Request, Response } from 'express';
+import { MongoError } from 'mongodb';
+import { ingredients } from '../common/types';
 import { Ingredient } from '../models/ingredients.model';
 
-export const getIngredients = (req: Request, res: Response): void => {
-  Ingredient.find({}, (_: any, ingredients: any) => {
+export const getIngredients = (_: Request, res: Response): void => {
+  Ingredient.find({}, (_: MongoError, ingredients: Array<ingredients>) => {
     res.send({ ingredients: ingredients[0].ingredients });
   });
 };
 
 export const getIngredientsByQuery = (req: Request, res: Response): void => {
   const query: string = _.startCase(req.params.query);
-  Ingredient.find({}, (_: any, ingredients: any) => {
+  Ingredient.find({}, (_: MongoError, ingredients: Array<ingredients>) => {
     const foundIngredients: Array<string> = ingredients[0].ingredients.filter(
       (ingredient: string) => {
         return ingredient.includes(query);
