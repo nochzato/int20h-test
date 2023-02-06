771bfda0864899b8bda2b131c9a99bf57dbcb147
Danil Nesterenko <nochzato@gmail.com>
2023-02-06 22:01:31 +0200

    added route to check for authentication

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-import { AuthenticateUser } from '../controllers/auth.controller';
+import {
+  AuthenticateUser,
+  checkAuthentication,
+} from '../controllers/auth.controller';
+import authenticateToken from '../middleware/authenticateToken';
 
 const router = express.Router();
 
 router.post('/auth', AuthenticateUser);
+
+router.get('/check-auth', authenticateToken, checkAuthentication);
 
 export default router;
