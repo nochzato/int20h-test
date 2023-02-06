021c57615ffd03c4f8cdc714766cdcf1032ebb57
Danil Nesterenko <nochzato@gmail.com>
2023-02-06 22:13:24 +0200

    list creation and update are now on same route

--- server/src/routes/list.router.ts
+++ server/src/routes/list.router.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import authenticateToken from '../middleware/authenticateToken';
-import { addRecipeToList, createNewList } from '../controllers/list.controller';
+import { createOrUpdateList } from '../controllers/list.controller';
 
 const router = express.Router();
 
-router.post('/lists', authenticateToken, createNewList);
-
-router.patch('/lists/:id', authenticateToken, addRecipeToList);
+router.post('/lists', authenticateToken, createOrUpdateList);
 
 export default router;
