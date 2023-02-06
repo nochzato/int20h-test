cf509d6fa95dfca2e29d68c386a21455d7dcafaf
Danil Nesterenko <nochzato@gmail.com>
2023-02-06 10:42:31 +0200

    duplicate emails are now not allowed

--- server/src/controllers/register.controller.ts
+++ server/src/controllers/register.controller.ts
@@ -4,15 +4,21 @@ import { User } from '../models/user.model';
 
 const saltRounds = 10;
 
-export const Register = (req: Request, res: Response) => {
-  bcrypt.hash(req.body.password, saltRounds, (_, password) => {
-    const newUser = new User({
-      email: req.body.email,
-      name: req.body.name,
-      password,
+export const Register = async (req: Request, res: Response) => {
+  const emailIsUsed = await User.findOne({ email: req.body.email });
+
+  if (emailIsUsed) {
+    res.sendStatus(400).end();
+  } else {
+    await bcrypt.hash(req.body.password, saltRounds, (_, password) => {
+      const newUser = new User({
+        email: req.body.email,
+        name: req.body.name,
+        password,
+      });
+      newUser.save().then((user) => {
+        res.send(user);
+      });
     });
-    newUser.save().then((user) => {
-      res.send(user);
-    });
-  });
+  }
 };
